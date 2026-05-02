c8177419023a48da910b9c9f0e7883efb04e053e
Jersy27 <153696495+Jersy27@users.noreply.github.com>
2026-05-01 22:09:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import pandas as pd
+import os
 
+app = Flask(__name__)
+
+archivo = "invitados.xlsx"
+
+# Crear Excel si no existe
+if not os.path.exists(archivo):
+    df = pd.DataFrame(columns=["Familia", "Cantidad", "Asistencia"])
+    df.to_excel(archivo, index=False)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+@app.route("/confirmar", methods=["POST"])
+def confirmar():
+    familia = request.form["familia"]
+    cantidad = request.form["cantidad"]
+    asistencia = request.form["asistencia"]
+
+    df = pd.read_excel(archivo)
+
+    nuevo = pd.DataFrame([[familia, cantidad, asistencia]],
+                         columns=["Familia", "Cantidad", "Asistencia"])
+
+    df = pd.concat([df, nuevo], ignore_index=True)
+    df.to_excel(archivo, index=False)
+
+    return redirect("/gracias")
+
+@app.route("/gracias")
+def gracias():
+    return render_template("gracias.html")
+
+app.run(debug=True)
